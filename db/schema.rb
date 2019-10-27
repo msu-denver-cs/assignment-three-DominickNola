6b64ce72f3928269eecf98ce213ccc8a88a57b05
Dominick Licciardi <dominick.nola@gmail.com>
2019-10-26 21:41:03 -0600

    added login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_203509) do
+ActiveRecord::Schema.define(version: 2019_10_27_030242) do
 
   create_table "car_parts", force: :cascade do |t|
     t.integer "car_id"
@@ -42,6 +42,18 @@ ActiveRecord::Schema.define(version: 2019_10_03_203509) do
     t.string "part_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
